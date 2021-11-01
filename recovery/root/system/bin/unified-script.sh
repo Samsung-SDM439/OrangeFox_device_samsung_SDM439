2881206ca31f908557674a0b2cb294fda8c01e6e
Sushrut1101 <guptasushrut@gmail.com>
2021-11-01 16:35:28 +0530

    SDM439: unified-script: Define m01q Props
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- recovery/root/system/bin/unified-script.sh
+++ recovery/root/system/bin/unified-script.sh
@@ -1,3 +1,13 @@
 #!/system/bin/sh
 # This script is needed to automatically set device props.
 
+load_m01q()
+{
+    resetprop "ro.product.model" "SM-M015G"
+    resetprop "ro.product.name" "m01q"
+    resetprop "ro.build.product" "m01q"
+    resetprop "ro.product.device" "m01q"
+    resetprop "ro.vendor.product.device" "m01q"
+
+}
+
