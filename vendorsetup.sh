f1c380b5b8ee84f2cd3b4f2188e6ea7cc07c51f5
Sushrut1101 <guptasushrut@gmail.com>
2021-11-01 17:08:33 +0530

    SDM439: Do Not Permit the user to Disable the Navigation Bar
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -73,6 +73,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export OF_STATUS_INDENT_RIGHT=52
 	export OF_CLOCK_POS=1
 	export OF_HIDE_NOTCH=1
+	export OF_ALLOW_DISABLE_NAVBAR=0
 	
 	# R11.1 Settings
 	export FOX_VERSION="R11.1_0"
